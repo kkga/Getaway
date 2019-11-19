ba54f9902c69647099e51c23895d64b46587cd39
Gadzhi Kharkharov <me@kkga.me>
2019-11-19 14:29:18 +0300

    upd

--- Lobby/Lobby.gd
+++ Lobby/Lobby.gd
@@ -1,14 +1,27 @@
 extends Control
 
+onready var name_line_edit = $ColorRect/VBoxContainer/CenterContainer/VBoxContainer/GridContainer/NameLineEdit
+onready var waiting_room = $WaitingRoomPopup
 
 
 func _ready() -> void:
-	$ColorRect/VBoxContainer/CenterContainer/VBoxContainer/GridContainer/NameLineEdit.text = SaveGame.save_data["Player_name"]
+	name_line_edit.text = SaveGame.save_data["Player_name"]
 
 
 func _on_HostButton_pressed() -> void:
 	Network.create_server()
+	show_waiting_room()
 
 
 func _on_JoinButton_pressed() -> void:
 	Network.connect_to_server()
+	show_waiting_room()
+
+
+func _on_NameLineEdit_text_changed(_new_text: String) -> void:
+	SaveGame.save_data["Player_name"] = name_line_edit.text
+	SaveGame.save_game()
+
+
+func show_waiting_room():
+	waiting_room.popup_centered()
--- Networking/Network.gd
+++ Networking/Network.gd
@@ -8,6 +8,8 @@ const DEFAULT_PORT: int = 4444
 const MAX_PLAYERS: int = 4
 
 var local_player_id: = 0
+sync var players = {}
+sync var player_data = {}
 
 
 func _ready() -> void:
@@ -19,9 +21,7 @@ func create_server() -> void:
 	var peer = NetworkedMultiplayerENet.new()
 	peer.create_server(DEFAULT_PORT, MAX_PLAYERS)
 	get_tree().set_network_peer(peer)
-	set_local_player_id()
-	print('server created')
-	print('i am %s' % local_player_id)
+	add_to_player_list()
 
 
 func connect_to_server() -> void:
@@ -29,18 +29,20 @@ func connect_to_server() -> void:
 	get_tree().connect("connected_to_server", self, "_on_connected_to_server")
 	peer.create_client(DEFAULT_IP, DEFAULT_PORT)
 	get_tree().set_network_peer(peer)
-	set_local_player_id()
 
 
-func set_local_player_id() -> void:
+func add_to_player_list() -> void:
 	local_player_id = get_tree().get_network_unique_id()
+	player_data = SaveGame.save_data
+	players[local_player_id] = player_data
 
 
 func _on_connected_to_server() -> void:
-	rpc("_send_player_info", local_player_id)
+	add_to_player_list()
+	rpc("_send_player_info", local_player_id, player_data)
 
 
-remote func _send_player_info(id) -> void:
+remote func _send_player_info(id, player_info) -> void:
 	if local_player_id == 1:
 		print(str(id) + " has connected")
 
