57ac76ee884564fd014f88883dab1e973e31c2c7
Gadzhi Kharkharov <me@kkga.me>
2019-11-21 19:11:53 +0300

    add meshes

--- Lobby/Lobby.gd
+++ Lobby/Lobby.gd
@@ -17,9 +17,7 @@ func show_waiting_room():
 	waiting_room.refresh_players(Network.players)
 
 
-
 # SIGNAL CALLBACKS
-
 
 func _on_HostButton_pressed() -> void:
 	Network.selected_port = int(port.text)
@@ -27,18 +25,15 @@ func _on_HostButton_pressed() -> void:
 	get_tree().call_group("host_only", "show")
 	show_waiting_room()
 
-
 func _on_JoinButton_pressed() -> void:
 	Network.selected_port = int(port.text)
 	Network.selected_ip = ip.text
 	Network.connect_to_server()
 	show_waiting_room()
 
-
 func _on_NameLineEdit_text_changed(_new_text: String) -> void:
 	SaveGame.save_data["Player_name"] = name_line_edit.text
 	SaveGame.save_game()
-
 
 func _on_StartButton_pressed() -> void:
 	Network.start_game()
